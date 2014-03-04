d0b7c7ce9ea6f132a130a0e16fcf26d476c41b68
Dan Nixon <dan@dan-nixon.com>
2014-03-04 16:26:27 +0000

    Fixed API issues, added sorting, moved to Mobileclient

---
+++ GetDeviceID.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+##	Google Play Music device ID grabber
+##	Used to get a device ID from the Google Music API to use with the Mobileclient
+##	dan-nixon.com
+##	Date: 04/03/2014
+
+import gmusicapi
+from getpass import getpass
+
+print "Username: ",
+user = raw_input()
+passwd = getpass()
+
+api = gmusicapi.Webclient()
+api.login(user, passwd)
+devices = api.get_registered_devices()
+
+for device in devices:
+	if device['type'] == 'PHONE':
+		print "Name:{d[name]}\tid:{d[id]}".format(d=device)
