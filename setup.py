da8745eca6ce315d73c68a1adccc3e4fb1daa8c6
Dan Nixon <dan@dan-nixon.com>
2015-10-23 19:56:50 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup
 
 setup(
     name='playmusiccl',
-    version='0.6.1',
+    version='0.6.2',
     entry_points = {
-	'console_scripts': ['playmusiccl=playmusiccl:run'],
+        'console_scripts': ['playmusiccl=playmusiccl:run'],
     },
     description='Text based command line client for Google Play Music',
     classifiers=[
-	'License :: OSI Approved :: Apache Software License',
-	'Natural Language :: English',
-	'Programming Language :: Python :: 2.7',
-	'Topic :: Multimedia :: Sound/Audio :: Players',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Multimedia :: Sound/Audio :: Players',
     ],
     keywords='google play music command line',
     url='http://github.com/DanNixon/PlayMusicCL',
@@ -20,8 +20,9 @@ setup(
     license='Apache',
     packages=['playmusiccl'],
     install_requires=[
-	'gmusicapi',
-	'pylast',
+        'gmusicapi',
+        'pylast',
     ],
     include_package_data=True,
-    zip_safe=False)
+    zip_safe=False
+)
