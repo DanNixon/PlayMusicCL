d8ee0710d28e7b5b16ccca70c2068b0273649ebb
Charles Beebe <charlescbeebe@gmail.com>
2016-05-10 14:51:18 -0400

    Fix GetDeviceID.py.
    Appears the shape of a device dict has changed since this script was
    last updated, i.e., 'type' is now 'deviceType'.
    
    Closes #42.

--- GetDeviceID.py
+++ GetDeviceID.py
@@ -18,6 +18,16 @@ api = gmusicapi.Webclient()
 api.login(user, passwd)
 devices = api.get_registered_devices()
 
+# `devices` is a list of dictionaries like this:
+# {u'carrier': u'Google',
+#  u'deviceType': 2,
+#  u'id': u'0x1111111111111111',
+#  u'lastAccessedFormatted': u'January 1, 1999',
+#  u'lastAccessedTimeMillis': 915148800,
+#  u'lastEventTimeMillis': 915148800,
+#  u'manufacturer': u'LGE',
+#  u'model': u'Nexus 5',
+#  u'name': u''}]
 for device in devices:
-	if device['type'] == 'PHONE':
-		print "Name:{d[name]}\tid:{d[id]}".format(d=device)
+	if device['deviceType'] == 2:
+		print "Device:{d[manufacturer]} {d[model]}\tid:{d[id]}".format(d=device)
